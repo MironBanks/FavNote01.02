147ecf54f33143c943066b07288b3675eaa665d3
Miron Banks <madapegarage@gmail.com>
2019-12-17 21:31:19 +0100

    global style theme

--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Button from '../../components/Button/Button';
+import GlobalStyle from '../../Theme/GlobalStyle';
 
 const Root = () => (
   <div>
+    <GlobalStyle />
     <h1>Hello Miron</h1>
     <Button width="500px">Close / Save</Button>
     <Button secondary>Remove</Button>
