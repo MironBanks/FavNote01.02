e0ef2406ac57ffc89c455cc204e1d5eb65c8399d
Miron Banks <madapegarage@gmail.com>
2019-12-17 05:22:22 +0100

    project structure settings plus first button

---
+++ src/views/Root/Root.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Button from '../../components/Button/Button';
+
+const Root = () => (
+  <div>
+    <h1>Hello Miron</h1>
+    <Button>Hello Hello</Button>
+  </div>
+);
+
+export default Root;
