e5d183eb433c3fad20267677864aced9e32aaec0
Miron Banks <madapegarage@gmail.com>
2019-12-18 20:29:41 +0100

    preview-head.html in storybook to add global styles

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,12 @@
 import { configure } from '@storybook/react';
 
+configure(require.context('../src', true, /\.stories\.js$/), module);
+/*
+import { configure } from '@storybook/react';
+
 function loadStories() {
   const req = require.context('../src/components', true, /\.stories\.js$/);
   req.keys().forEach(filename => req(filename));
 }
 
-configure(loadStories, module);
+configure(loadStories, module); */
--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -7,7 +7,7 @@ const Button = styled.button`
   height: 47px;
   border: none;
   border-radius: 50px;
-  font-family: 'Montserrat ';
+  font-family: 'Montserrat';
   font-weight: 500;
   font-size: 16px;
   text-transform: uppercase;
