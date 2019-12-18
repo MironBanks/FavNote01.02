3291780192b6e11bc515b8b4035612e1f84f99c4
Miron Banks <madapegarage@gmail.com>
2019-12-18 21:48:13 +0100

    adding theme provider

--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,13 +1,25 @@
 import React from 'react';
-import Button from '../../components/Button/Button';
+import { TheamProvider } from 'styled-components';
+import Button from '../../components/atoms/Button/Button';
 import GlobalStyle from '../../Theme/GlobalStyle';
+
+// First ThemeProvider - color => black
+const theme = {
+  primary: 'black',
+};
+// bacground-color: ${(props) => props.theme.primary};
+// End ThemeProvider
 
 const Root = () => (
   <div>
     <GlobalStyle />
-    <h1>Hello Miron</h1>
-    <Button width="500px">Close / Save</Button>
-    <Button secondary>Remove</Button>
+    <TheamProvider theme={theme}>
+      <>
+        <h1>Hello Miron</h1>
+        <Button width="500px">Close / Save</Button>
+        <Button secondary>Remove</Button>
+      </>
+    </TheamProvider>
   </div>
 );
 
