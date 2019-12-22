b849d2547a43b8ed5cb1ae1629ddc9ffd3b4172a
Miron Banks <madapegarage@gmail.com>
2019-12-23 00:50:35 +0100

    global theme

---
+++ src/Theme/mainTheme.js
@@ -0,0 +1,18 @@
+export const theme = {
+  primary: 'hsl(49, 100%, 58%)',
+  secondary: 'hsl(196, 83%, 75%)',
+  tertiary: 'hsl(106, 47%, 64%)',
+  grey100: 'hsl(0, 0%, 96%)',
+  grey200: 'hsl(0, 0%, 90%)',
+  black: 'hsl(0, 0%, 0%)',
+  light: 300,
+  bold: 300,
+};
+
+// First ThemeProvider - color => black
+/* const theme = {
+  primary: 'black',
+}; */
+
+// bacground-color: ${(props) => props.theme.primary};
+// End ThemeProvider
--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import { TheamProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 import Button from '../../components/atoms/Button/Button';
 import GlobalStyle from '../../Theme/GlobalStyle';
-
-// First ThemeProvider - color => black
-const theme = {
-  primary: 'black',
-};
-// bacground-color: ${(props) => props.theme.primary};
-// End ThemeProvider
+import { theme } from '../../Theme/mainTheme';
 
 const Root = () => (
   <div>
     <GlobalStyle />
-    <TheamProvider theme={theme}>
+    <ThemeProvider theme={theme}>
       <>
         <h1>Hello Miron</h1>
         <Button width="500px">Close / Save</Button>
         <Button secondary>Remove</Button>
       </>
-    </TheamProvider>
+    </ThemeProvider>
   </div>
 );
 
