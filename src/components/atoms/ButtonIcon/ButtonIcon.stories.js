c667a4c7cfa9adb82f655ad3834afc3b397a43db
Miron Banks <madapegarage@gmail.com>
2019-12-23 04:03:52 +0100

    buttonIcon yellow Backgroung

--- src/components/atoms/ButtonIcon/ButtonIcon.stories.js
+++ src/components/atoms/ButtonIcon/ButtonIcon.stories.js
@@ -1,1 +1,53 @@
-// ButtonIcon story
+import React from 'react';
+import styled from 'styled-components';
+import bulbIcon from '../../../assets/icons/bulb.svg';
+import logoutIcon from '../../../assets/icons/logout.svg';
+import penIcon from '../../../assets/icons/pen.svg';
+import plusIcon from '../../../assets/icons/plus.svg';
+import twitterIcon from '../../../assets/icons/twitter.svg';
+import ButtonIcon from './ButtonIcon';
+
+const YellowBackground = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 500px;
+  height: 500px;
+  background: ${({ theme }) => theme.primary};
+`;
+
+export default {
+  component: ButtonIcon,
+  title: 'ButtonIcon',
+};
+
+export const Bulb = () => (
+  <YellowBackground>
+    <ButtonIcon icon={bulbIcon} />
+  </YellowBackground>
+);
+export const Active = () => (
+  <YellowBackground>
+    <ButtonIcon active icon={bulbIcon} />
+  </YellowBackground>
+);
+export const Logout = () => (
+  <YellowBackground>
+    <ButtonIcon icon={logoutIcon} />
+  </YellowBackground>
+);
+export const Pen = () => (
+  <YellowBackground>
+    <ButtonIcon icon={penIcon} />
+  </YellowBackground>
+);
+export const Plus = () => (
+  <YellowBackground>
+    <ButtonIcon icon={plusIcon} />
+  </YellowBackground>
+);
+export const Twitter = () => (
+  <YellowBackground>
+    <ButtonIcon icon={twitterIcon} />
+  </YellowBackground>
+);
