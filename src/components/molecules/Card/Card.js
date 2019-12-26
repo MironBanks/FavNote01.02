9495b929dfcb1dba06ef46dd953eab2f125cf6cf
Miron Banks <madapegarage@gmail.com>
2019-12-26 14:46:07 +0800

    card css

--- src/components/molecules/Card/Card.js
+++ src/components/molecules/Card/Card.js
@@ -1,20 +1,56 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import Paragraph from '../../atoms/Paragraph/Paragraph';
 import Heading from '../../atoms/Heading/Heading';
+import Button from '../../atoms/Button/Button';
 
-const StyleWrapper = styled.div`
+const StyledWrapper = styled.div`
+  min-height: 380px;
+  box-shadow: 0 10px 30px -10px hsla(0, 0%, 0%, 0.1);
+  border-radius: 10px;
+  overflow: hidden;
+  position: relative;
+  display: grid;
+  grid-template-rows: 0.25fr 1fr;
+`;
+
+const InnerWrapper = styled.div`
   padding: 17px 30px;
+  background-color: ${({ yellow, theme }) => (yellow ? theme.primary : 'white')};
+
+  ${({ flex }) =>
+    flex &&
+    css`
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+    `}
+`;
+
+const DateInfo = styled(Paragraph)`
+  margin: 0 0 5px;
+  font-weight: ${({ theme }) => theme.bold};
+  font-size: ${({ theme }) => theme.fontSize.xs};
+`;
+
+const StyledHeading = styled(Heading)`
+  margin: 5px 0 0;
 `;
 
 const Card = () => (
-  <StyleWrapper>
-    <Heading>Hello Miron</Heading>
-    <Paragraph>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eu ante ut justo convallis
-      aliquet ut id eros. Nunc a fringilla magna, varius posuere dolor. Nullam eu congue erat.
-    </Paragraph>
-  </StyleWrapper>
+  <StyledWrapper>
+    <InnerWrapper yellow>
+      <StyledHeading>Hello Miron</StyledHeading>
+      <DateInfo>3 days</DateInfo>
+    </InnerWrapper>
+    <InnerWrapper flex>
+      <Paragraph>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit nemo ducimus fuga
+        repellendus illum
+      </Paragraph>
+      <Button secondary>REMOVE</Button>
+    </InnerWrapper>
+  </StyledWrapper>
 );
 
 export default Card;
