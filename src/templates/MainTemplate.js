a6e7d6086d47e021f98ce8822e0a01f6f7bb9c3a
Miron Banks <madapegarage@gmail.com>
2020-01-01 13:34:01 +0800

    adding react router

---
+++ src/templates/MainTemplate.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from '../Theme/GlobalStyle';
+import { theme } from '../Theme/mainTheme';
+
+const MainTemplate = ({ children }) => (
+  <div>
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>{children}</ThemeProvider>
+  </div>
+);
+
+MainTemplate.propTypes = {
+  children: PropTypes.element.isRequired,
+};
+
+export default MainTemplate;
