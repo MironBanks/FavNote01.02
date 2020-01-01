13a51798c2c05c618d6db1e4aa3d1a18a3dd5418
Miron Banks <madapegarage@gmail.com>
2020-01-01 12:34:51 +0800

    rebuilding card component, adding icons and link

--- src/components/molecules/Card/Card.js
+++ src/components/molecules/Card/Card.js
@@ -4,6 +4,7 @@ import styled, { css } from 'styled-components';
 import Paragraph from '../../atoms/Paragraph/Paragraph';
 import Heading from '../../atoms/Heading/Heading';
 import Button from '../../atoms/Button/Button';
+import LinkIcon from '../../../assets/icons/link.svg';
 
 const StyledWrapper = styled.div`
   min-height: 380px;
@@ -16,8 +17,13 @@ const StyledWrapper = styled.div`
 `;
 
 const InnerWrapper = styled.div`
+  position: relative;
   padding: 17px 30px;
   background-color: ${({ activeColor, theme }) => (activeColor ? theme[activeColor] : 'white')};
+
+  :first-of-type {
+    z-index: 99999;
+  }
 
   ${({ flex }) =>
     flex &&
@@ -38,11 +44,37 @@ const StyledHeading = styled(Heading)`
   margin: 5px 0 0;
 `;
 
+const StyledAvatar = styled.img`
+  width: 86px;
+  height: 86px;
+  border: 5px solid ${({ theme }) => theme.twitter};
+  border-radius: 50px;
+  position: absolute;
+  right: 25px;
+  top: 25px;
+`;
+
+const StyledLinkButton = styled.a`
+  display: block;
+  width: 47px;
+  height: 47px;
+  border-radius: 50px;
+  background: white url(${LinkIcon}) no-repeat;
+  background-size: 60%;
+  background-position: 50%;
+  position: absolute;
+  right: 25px;
+  top: 50%;
+  transform: translateY(-50%);
+`;
+
 const Card = ({ cardType }) => (
   <StyledWrapper>
     <InnerWrapper activeColor={cardType}>
       <StyledHeading>Hello Miron</StyledHeading>
       <DateInfo>3 days</DateInfo>
+      {cardType === 'twitter' && <StyledAvatar src="https://avatars.io/twitter/hello_roman" />}
+      {cardType === 'article' && <StyledLinkButton href="https://youtube.com/helloroman" />}
     </InnerWrapper>
     <InnerWrapper flex>
       <Paragraph>
--- src/components/molecules/Card/Card.stories.js
+++ src/components/molecules/Card/Card.stories.js
@@ -6,6 +6,6 @@ export default {
   title: 'Card',
 };
 
-export const Primary = () => <Card cardType="note" />;
-export const Secondary = () => <Card cardType="twitter" />;
-export const Tertiary = () => <Card cardType="article" />;
+export const Note = () => <Card />;
+export const Twitter = () => <Card cardType="twitter" />;
+export const Article = () => <Card cardType="article" />;
