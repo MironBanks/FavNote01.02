8a077479f33a8e56d59f0465122f1d971723399e
Miron Banks <madapegarage@gmail.com>
2020-01-02 12:04:58 +0800

    static user page template

--- src/templates/UserPageTemplate.js
+++ src/templates/UserPageTemplate.js
@@ -2,18 +2,46 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import Sidebar from '../components/organisms/Sidebar/Sidebar';
+import Input from '../components/atoms/Input/Input';
+import Heading from '../components/atoms/Heading/Heading';
+import Paragraph from '../components/atoms/Paragraph/Paragraph';
 
-const StyledGridWrapper = styled.div`
+const StyledWrapper = styled.div`
+  padding: 25px 150px 25px 70px;
+`;
+
+const StyledGrid = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   grid-gap: 85px;
-  padding: 25px 150px 25px 70px;
+`;
+
+const StyledPageHeader = styled.div`
+  margin: 25px 0px 50px 0px;
+`;
+
+const StyledHeading = styled(Heading)`
+  margin: 25px 0 0 0;
+`;
+
+const StyledParagraph = styled(Paragraph)`
+  margin: 0px;
+  font-weight: ${({ theme }) => theme.bold};
 `;
 
 const UserPageTemplate = ({ children, pageType }) => (
   <>
     <Sidebar pageType={pageType} />
-    <StyledGridWrapper>{children}</StyledGridWrapper>
+    <StyledWrapper>
+      <StyledPageHeader>
+        <Input search placeholder="Search" />
+        <StyledHeading big as="h1">
+          Notes
+        </StyledHeading>
+        <StyledParagraph>6 notes</StyledParagraph>
+      </StyledPageHeader>
+      <StyledGrid>{children}</StyledGrid>
+    </StyledWrapper>
   </>
 );
 
