ab9dc1fcc9272adb8b883e2077e4517614feea5e
Miron Banks <madapegarage@gmail.com>
2020-01-03 22:54:25 +0800

    cleaning errors

--- src/templates/MainTemplate.js
+++ src/templates/MainTemplate.js
@@ -12,7 +12,7 @@ const MainTemplate = ({ children }) => (
 );
 
 MainTemplate.propTypes = {
-  children: PropTypes.element.isRequired,
+  children: PropTypes.oneOfType([PropTypes.element, PropTypes.node]).isRequired,
 };
 
 export default MainTemplate;
