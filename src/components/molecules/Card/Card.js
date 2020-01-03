4b23a922e2bb7db7c1b3bcd194d304be658fa97e
Miron Banks <madapegarage@gmail.com>
2020-01-03 21:24:34 +0800

    fixing single cards

--- src/components/molecules/Card/Card.js
+++ src/components/molecules/Card/Card.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
 import styled, { css } from 'styled-components';
 import Paragraph from '../../atoms/Paragraph/Paragraph';
 import Heading from '../../atoms/Heading/Heading';
@@ -22,7 +23,7 @@ const InnerWrapper = styled.div`
   background-color: ${({ activeColor, theme }) => (activeColor ? theme[activeColor] : 'white')};
 
   :first-of-type {
-    z-index: 99999;
+    z-index: 9999;
   }
 
   ${({ flex }) =>
@@ -47,7 +48,7 @@ const StyledHeading = styled(Heading)`
 const StyledAvatar = styled.img`
   width: 86px;
   height: 86px;
-  border: 5px solid ${({ theme }) => theme.twitter};
+  border: 5px solid ${({ theme }) => theme.twitters};
   border-radius: 50px;
   position: absolute;
   right: 25px;
@@ -68,23 +69,43 @@ const StyledLinkButton = styled.a`
   transform: translateY(-50%);
 `;
 
-const Card = ({ cardType, title, created, twitterName, articleUrl, content }) => (
-  <StyledWrapper>
-    <InnerWrapper activeColor={cardType}>
-      <StyledHeading>{title}</StyledHeading>
-      <DateInfo>{created}</DateInfo>
-      {cardType === 'twitter' && <StyledAvatar src={`https://avatars.io/twitter/${twitterName}`} />}
-      {cardType === 'article' && <StyledLinkButton href={articleUrl} />}
-    </InnerWrapper>
-    <InnerWrapper flex>
-      <Paragraph>{content}</Paragraph>
-      <Button secondary>REMOVE</Button>
-    </InnerWrapper>
-  </StyledWrapper>
-);
+class Card extends Component {
+  // eslint-disable-next-line react/state-in-constructor
+  state = {
+    redirect: false,
+  };
+
+  handleCardClick = () => this.setState({ redirect: true });
+
+  render() {
+    const { id, cardType, title, created, twitterName, articleUrl, content } = this.props;
+    const { redirect } = this.state;
+
+    if (redirect) {
+      return <Redirect to={`${cardType}/${id}`} />;
+    }
+    return (
+      <StyledWrapper onClick={this.handleCardClick}>
+        <InnerWrapper activeColor={cardType}>
+          <StyledHeading>{title}</StyledHeading>
+          <DateInfo>{created}</DateInfo>
+          {cardType === 'twitters' && (
+            <StyledAvatar src={`https://avatars.io/twitter/${twitterName}`} />
+          )}
+          {cardType === 'articles' && <StyledLinkButton href={articleUrl} />}
+        </InnerWrapper>
+        <InnerWrapper flex>
+          <Paragraph>{content}</Paragraph>
+          <Button secondary>REMOVE</Button>
+        </InnerWrapper>
+      </StyledWrapper>
+    );
+  }
+}
 
 Card.propTypes = {
-  cardType: PropTypes.oneOf(['note', 'twitter', 'article']),
+  id: PropTypes.string.isRequired,
+  cardType: PropTypes.oneOf(['notes', 'twitters', 'articles']),
   title: PropTypes.string.isRequired,
   created: PropTypes.string.isRequired,
   twitterName: PropTypes.string,
@@ -93,7 +114,7 @@ Card.propTypes = {
 };
 
 Card.defaultProps = {
-  cardType: 'note',
+  cardType: 'notes',
   twitterName: null,
   articleUrl: null,
 };
--- src/views/DetailsPage.js
+++ src/views/DetailsPage.js
@@ -1,10 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { routes } from '../routes/index';
 import DetailsTemplate from '../templates/DetailsTemplate';
 
-const DetailsPage = () => (
-  <>
-    <DetailsTemplate />
-  </>
-);
+class DetailsPage extends Component {
+  // eslint-disable-next-line react/state-in-constructor
+  state = {
+    pageType: 'notes',
+  };
+
+  componentDidMount() {
+    const { match } = this.props;
+
+    switch (match.path) {
+      case routes.twitter:
+        this.setState({ pageType: 'twitters' });
+        break;
+      case routes.note:
+        this.setState({ pageType: 'notes' });
+        break;
+      case routes.article:
+        this.setState({ pageType: 'articles' });
+        break;
+      default:
+        console.log('Something went wrong with matching paths');
+    }
+  }
+
+  render() {
+    const { pageType } = this.state;
+
+    return (
+      <DetailsTemplate pageType={pageType}>
+        <p>{pageType}</p>
+      </DetailsTemplate>
+    );
+  }
+}
+
+DetailsPage.propTypes = {
+  match: PropTypes.string.isRequired,
+};
 
 export default DetailsPage;
