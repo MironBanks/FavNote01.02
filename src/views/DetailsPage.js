a259555f9e18f62d574392e17f427041d5738d8d
Miron Banks <madapegarage@gmail.com>
2020-01-08 23:54:19 +0700

    render and animation

--- src/views/DetailsPage.js
+++ src/views/DetailsPage.js
@@ -3,7 +3,7 @@ import DetailsTemplate from '../templates/DetailsTemplate';
 
 const dummyArticle = {
   id: 1,
-  title: 'Wake me up when Vue ends',
+  title: 'Wake me up when Vue...',
   content:
     'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
   twitterName: 'hello_roman',
